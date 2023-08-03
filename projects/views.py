aac1c684b0f601e56ac13944e74f6f46d555fac2
SKrpio <134825964+SKrpio@users.noreply.github.com>
2023-08-03 15:43:38 +1000

    lesson 3/6

--- projects/views.py
+++ projects/views.py
@@ -1,21 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project, Pledge
-from .serializers import ProjectSerializer, PledgeSerializer
+from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
 from django.http import Http404
 from rest_framework import status
+
 
 class ProjectList(APIView):
 
     def get(self, request):
         projects = Project.objects.all()
-        serializer = ProjectSerializer(projects, many=True)
+        serializer = ProjectDetailSerializer(projects, many=True)
         return Response(serializer.data)
     
     def post(self, request):
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(owner=request.user)
             return Response(
                 serializer.data,
                 status=status.HTTP_201_CREATED
